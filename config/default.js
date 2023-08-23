9714faf3430b636085f394f06da5135468ea32b3
Djess-V <e.ageevets@yandex.ru>
2023-08-23 12:05:02 +0700

    completed 1 task

--- config/default.js
+++ config/default.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
   apps: {
     web: {
       port: 3000,
     },
   },
-  mongodb: 'mongodb://mongodb/mongodb',
+  mongodb: `mongodb+srv://eageevets:${process.env.PASSWORD}@cluster0.dbshknz.mongodb.net/?retryWrites=true&w=majority`,
 };
