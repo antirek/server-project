a678f20f85f18a3958d28659b886819464bcde2a
Kenaye <56346685+Tavien@users.noreply.github.com>
2024-02-08 15:16:48 +0700

    Add group model
    Added group model for ORM

--- models/index.js
+++ models/index.js
@@ -4,17 +4,20 @@ mongoose.Promise = Promise;
 
 const settingsConnection = mongoose.createConnection(config.get('mongodb'));
 
-const {serverSchema} = require('./server');
-const {userActionSchema} = require('./user-action')
-const {taskSchema} = require('./task')
+const { serverSchema } = require('./server');
+const { groupSchema } = require('./group');
+const { userActionSchema } = require('./user-action')
+const { taskSchema } = require('./task')
 
 const Server = settingsConnection.model('servers', serverSchema);
+const Group = settingsConnection.model('groups', groupSchema);
 const UserAction = settingsConnection.model('useractions', userActionSchema);
 const Task = settingsConnection.model('tasks', taskSchema);
 
 
 module.exports = {
   Server,
+  Group,
   UserAction,
   Task,
 };
